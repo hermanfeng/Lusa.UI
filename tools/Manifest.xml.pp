bde4262e036452a01ee9ae27e61b0e4ab723f946
hermanfeng <hfeng@seismic.com>
2019-03-06 19:10:31 +0800

    update

---
+++ tools/Manifest.xml.pp
@@ -0,0 +1,5 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<Bundle xmlns="urn:uiosp-bundle-manifest-2.0" Name="$AssemblyName$" SymbolicName="$AssemblyName$" Version="1.0.0.0" InitializedState="Active">
+  <Runtime>
+  </Runtime>
+<Bundle>
--- tools/PackAllHere.cmd
+++ tools/PackAllHere.cmd
@@ -1,4 +1,4 @@
 set local=%~dp0
 nuget pack Lusa.UI.1.0.0.nuspec
-
+nuget pack Lusa.UI.App.1.0.0.nuspec
 pause
