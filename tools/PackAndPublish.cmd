af3b989eb7f85aa2222584a54abb99ae1220927f
hermanfeng <hfeng@seismic.com>
2019-03-07 17:33:06 +0800

    update

--- tools/PackAndPublish.cmd
+++ tools/PackAndPublish.cmd
@@ -1,4 +1,10 @@
 echo %1
+UpdatePackageVersion.exe %1
+
+nuget pack Lusa.UI.PaneView.nuspec
+nuget pack Lusa.UI.Startup.nuspec
+nuget pack Lusa.UI.Host.nuspec
+
 nuget push -src http://hermanlinux.seismic-dev.com -ApiKey 123 Lusa.UI.Host.%1.nupkg
 nuget push -src http://hermanlinux.seismic-dev.com -ApiKey 123 Lusa.UI.PaneView.%1.nupkg
 nuget push -src http://hermanlinux.seismic-dev.com -ApiKey 123 Lusa.UI.Startup.%1.nupkg
